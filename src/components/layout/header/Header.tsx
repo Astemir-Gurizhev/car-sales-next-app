95d6bf1c853c5d2c1a7955fe53702bb24c5f14f4
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-02-24 17:50:14 +0300

    Added scss and added font titillium

--- src/components/layout/header/Header.tsx
+++ src/components/layout/header/Header.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link'
 import React from 'react'
-import styles from './Header.module.css'
+import styles from './Header.module.scss'
 import { useRouter } from 'next/router'
 
 const Header = () => {
