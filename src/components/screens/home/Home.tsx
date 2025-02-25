314adfc7f7c4921feda24373b71f17c134b9d294
Astemir-Gurizhev <gurizhev.astemir@gmail.com>
2025-02-25 17:20:56 +0300

    Added meta component

--- src/components/screens/home/Home.tsx
+++ src/components/screens/home/Home.tsx
@@ -2,7 +2,7 @@ import Layout from '@/components/layout/Layout'
 
 export const Home = () => {
 	return (
-		<Layout>
+		<Layout title='Home' description='We like to sale cars'>
 			<h1>salam</h1>
 		</Layout>
 	)
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image'
 
 const NotFound = () => {
 	return (
-		<Layout>
+		<Layout title='Not Found' description='Not Found'> 
 			<Head>
 				<title>Not Found</title>
 			</Head>
